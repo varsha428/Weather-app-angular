593158e5e1ca5a66fba0c794127ca15b7814dcce
varsha428 <varshumandloi@gmail.com>
2025-05-24 07:11:37 +0530

    Routes and other configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { WeatherComponent } from './weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ // ✅ Ensure 'routes' is exported
+  { path: '', redirectTo: 'weather', pathMatch: 'full' },
+  { path: 'weather', component: WeatherComponent },
+];
